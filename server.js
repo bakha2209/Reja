9a08966d303973b5ee2fb5ff0a9e8198d1dac46d
bakha2209 <bakhodir2209@gmail.com>
2023-09-30 02:01:48 +0900

    MongoDB connection, task e in train.js

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs =require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err,data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  } else {
-    user=JSON.parse(data)
-  }
-});
-// 1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+const mongodb = require("mongodb")
+let db;
 
-// 2 Session code
+const connectionString = "mongodb+srv://bakhodir2209:913686073m@cluster0.e30hwsa.mongodb.net/"
+mongodb.connect(connectionString,{
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+}, (err,client)=> {
 
-
-// 3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-  //console.log(req.body);
-  console.log(req)
-  res.json({test: "sucess"});
+    if(err) console.log("Error on connection MongoDB");
+    else {
+        console.log("MongoDB connection succed")
+        module.exports = client;
+        const app = require("./app")
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+          console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+        });  
+    }
 });
 
-app.get("/author", (req,res) =>{
-  res.render("author", {user: user});
-})
 
-app.get("/", function (req, res) {
-  res.render("harid");
-});
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}`);
-});
--- train.js
+++ train.js
@@ -1,85 +1,98 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yaxshi talaba boling",//0-20
-  "togri boshliq tanlang va koproq xato qiling", //20-30
-  "ozingizga ishlashni boshlang", //30-40
-  "siz kuchli bolgan narsalarni qiling",//40-50
-  "yoshlarga investitsiya qiling",//50-60
-  "endi dam oling, boldida endi" //60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba boling",//0-20
+//   "togri boshliq tanlang va koproq xato qiling", //20-30
+//   "ozingizga ishlashni boshlang", //30-40
+//   "siz kuchli bolgan narsalarni qiling",//40-50
+//   "yoshlarga investitsiya qiling",//50-60
+//   "endi dam oling, boldida endi" //60
+// ];
 
-// function maslahatBering(a,callback) {
-//     if(typeof a !=='number') callback("insert a number",null);
-//     else if (a<=20) callback(null,list[0]);
-//     else if (a>20 && a<=30) callback(null, list[1]);
-//     else if (a>30 && a<=40) callback(null, list[2]);
-//     else if (a>40 && a<=50) callback(null, list[3]);
-//     else if (a>50 && a<=60) callback(null, list[4]);
+// // function maslahatBering(a,callback) {
+// //     if(typeof a !=='number') callback("insert a number",null);
+// //     else if (a<=20) callback(null,list[0]);
+// //     else if (a>20 && a<=30) callback(null, list[1]);
+// //     else if (a>30 && a<=40) callback(null, list[2]);
+// //     else if (a>40 && a<=50) callback(null, list[3]);
+// //     else if (a>50 && a<=60) callback(null, list[4]);
+// //     else {
+// //       setTimeout(function () {
+// //         callback(null,list[5]);
+// //       },5000)
+// //     }
+// // }
+
+// // console.log("passed here 0");
+// // maslahatBering(65,(err,data) => {
+// //   if(err) console.log('ERROR:',err);
+// //   else {console.log('javob:',data)} 
+// // })
+
+// // console.log("passed here 1");
+
+
+
+// // asyn 
+// async function maslahatBering(a) {
+//     if(typeof a !=='number') throw new Error ("insert a number");
+//     else if (a<=20) return list[0];
+//     else if (a>20 && a<=30) return list[1];
+//     else if (a>30 && a<=40) return list[2];
+//     else if (a>40 && a<=50) return list[3];
+//     else if (a>50 && a<=60) return list[4];
 //     else {
-//       setTimeout(function () {
-//         callback(null,list[5]);
-//       },5000)
+//       return new Promise((resolve,reject) => {
+//         setTimeout(function () {
+//           resolve(list[5]);
+//         },5000) // agar shu yerda setInterval ni ishlatganimizda bir marta ishlaydi va toxtaydi
+//                 // shuning uchun uni callback da ishlatsak ishlaydi
+//       })
+      
 //     }
 // }
 
-// console.log("passed here 0");
-// maslahatBering(65,(err,data) => {
-//   if(err) console.log('ERROR:',err);
-//   else {console.log('javob:',data)} 
-// })
-
-// console.log("passed here 1");
-
-
-
-// asyn 
-async function maslahatBering(a) {
-    if(typeof a !=='number') throw new Error ("insert a number");
-    else if (a<=20) return list[0];
-    else if (a>20 && a<=30) return list[1];
-    else if (a>30 && a<=40) return list[2];
-    else if (a>40 && a<=50) return list[3];
-    else if (a>50 && a<=60) return list[4];
-    else {
-      return new Promise((resolve,reject) => {
-        setTimeout(function () {
-          resolve(list[5]);
-        },5000) // agar shu yerda setInterval ni ishlatganimizda bir marta ishlaydi va toxtaydi
-                // shuning uchun uni callback da ishlatsak ishlaydi
-      })
-      
-    }
-}
-
-// call via tnen/catch
-// console.log("passed here 0");
-// maslahatBering(65)
-//   .then(data => {
-//     maslahatBering(30)
-//     .then(data => {
-//       console.log("javob:",data);
-//     })
-//     .catch(err => {
-//       console.log("Error:",err);
-//     })
+// // call via tnen/catch
+// // console.log("passed here 0");
+// // maslahatBering(65)
+// //   .then(data => {
+// //     maslahatBering(30)
+// //     .then(data => {
+// //       console.log("javob:",data);
+// //     })
+// //     .catch(err => {
+// //       console.log("Error:",err);
+// //     })
   
-//   console.log("passed here 1");
-//     console.log("javob:",data);
-//   })
-//   .catch(err => {
-//     console.log("Error:",err);
-//   })
+// //   console.log("passed here 1");
+// //     console.log("javob:",data);
+// //   })
+// //   .catch(err => {
+// //     console.log("Error:",err);
+// //   })
 
-// console.log("passed here 1");
+// // console.log("passed here 1");
 
 
-// call via await
-async function run () {
-    let javob = await maslahatBering(20);
-    console.log(javob);
-    javob = await maslahatBering(31);
-    console.log(javob);
-    javob = await maslahatBering(65);
-    console.log(javob);
+// // call via await
+// async function run () {
+//     let javob = await maslahatBering(20);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(65);
+//     console.log(javob);
+// }
+// run()
+
+function getReverse(hello) {
+    letter_text = hello.split("");
+    
+    const new_list=[]
+    letter_text.forEach(ele => {
+      new_list.unshift(ele);
+    });
+    console.log(new_list.join(""))
 }
-run()
+
+getReverse("world");
+
