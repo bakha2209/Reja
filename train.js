37f91848ff47958aa97aeae3e2292834ca8133a2
bakha2209 <bakhodir2209@gmail.com>
2023-10-02 20:34:20 +0900

    frontend reja mini loyihasi

--- train.js
+++ train.js
@@ -98,41 +98,77 @@
 
 //Secar challenge
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-let code = 2;
+// const alphabet = "abcdefghijklmnopqrstuvwxyz";
+// let code = 2;
 
 
-function decodeMessage(message,secret) {
-  const letter_message = message.split("");
-  const letter_alphabet = alphabet.split("");
-  const new_list =[]
-  for (value of letter_message) {
-    if (letter_alphabet.includes(value)) {
-      //console.log(letter_alphabet.indexOf(value));
-      const new_index = letter_alphabet.indexOf(value) + secret;
-      if (new_index>25) {
-        const new_index_1 = new_index - 26;
-        new_list.push(letter_alphabet[new_index_1])
-      }else if(new_index<0){
-        const new_index_2 = new_index + 26;
-        new_list.push(letter_alphabet[new_index_2]) 
-      }else {new_list.push(letter_alphabet[new_index])};
+// function decodeMessage(message,secret) {
+//   const letter_message = message.split("");
+//   const letter_alphabet = alphabet.split("");
+//   const new_list =[]
+//   for (value of letter_message) {
+//     if (letter_alphabet.includes(value)) {
+//       //console.log(letter_alphabet.indexOf(value));
+//       const new_index = letter_alphabet.indexOf(value) + secret;
+//       if (new_index>25) {
+//         const new_index_1 = new_index - 26;
+//         new_list.push(letter_alphabet[new_index_1])
+//       }else if(new_index<0){
+//         const new_index_2 = new_index + 26;
+//         new_list.push(letter_alphabet[new_index_2]) 
+//       }else {new_list.push(letter_alphabet[new_index])};
       
-    } else {new_list.push(value)}
-  }
-  return new_list.join("")
+//     } else {new_list.push(value)}
+//   }
+//   return new_list.join("")
+// }
+
+
+
+// let message = "yes, how are you doing? my general!";
+// let secret_msg = decodeMessage(message,code);
+// console.log("Secret =>", secret_msg)
+
+// setTimeout(() => {
+//   code*=-1;
+//   const message_encoded = decodeMessage(secret_msg,code);
+//   console.log("Original =>", message_encoded)
+// },5000)
+
+
+function findDoublers(word) {
+  const letter_word = word.split("");
+
+  const new_list = new Set(letter_word)
+  const filteredElements = letter_word.filter(ele => {
+    if (new_list.has(ele)) {
+      new_list.delete(ele)} else {
+        return ele;
+      }
+    
+  } )
+  console.log(filteredElements)  
+  
+    
+    
 }
 
+findDoublers("hello")
 
+// function findDoublers(str) {
+//   // Bo'sh ob'ektni yaratamiz, harflarni san'aymiz
+//   const charCount = {};
 
-let message = "yes, how are you doing? my general!";
-let secret_msg = decodeMessage(message,code);
-console.log("Secret =>", secret_msg)
+//   // Stringni aylanib o'tamiz va harflarni san'aymiz
+//   for (const char of str) {
+//       if (charCount[char]) {
+//           // Agar harf allaqachon sanalgan bo'lsa, true qaytar
+//           return true;
+//       }
+//       charCount[char] = 1;
+//   }
 
-setTimeout(() => {
-  code*=-1;
-  const message_encoded = decodeMessage(secret_msg,code);
-  console.log("Original =>", message_encoded)
-},5000)
-
-
+//   // Harf bir marta ham takrorlanmagan bo'lsa, false qaytar
+//   return false
+// }
+// findDoublers("hello")
